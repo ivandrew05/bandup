b4792a1521d8109ee5997e7339dfd17f3433ec29
IvanDrew <ivandrew05@gmail.com>
2019-10-05 23:42:55 +0800

    teacher detail view

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,6 +1,7 @@
-from django.urls import path
-from .views import PostCreateView, PostUpdateView, PostDeleteView, UserPostListView, TeacherListView, TeacherDetailView
 from . import views
+from django.urls import path
+from .views import (PostCreateView, PostUpdateView, PostDeleteView, UserPostListView, 
+TeacherListView, TeacherDetailView, TeacherUpdateView, TeacherDeleteView)
 
 
 urlpatterns = [
@@ -8,9 +9,14 @@ urlpatterns = [
     path("", TeacherListView.as_view(), name="myapp-home"),
     path("user/<str:username>", UserPostListView.as_view(), name="user-posts"),
     # path("post/<int:pk>/", PostDetailView.as_view(), name="post-detail"),
-    path("post/<int:pk>/", TeacherDetailView.as_view(), name="teacher-detail"),
+    path("teacher/<int:pk>/", TeacherDetailView.as_view(), name="teacher-detail"),
     path("post/new/", PostCreateView.as_view(), name="post-create"),
-    path("post/<int:pk>/update/", PostUpdateView.as_view(), name="post-update"),
+#     path("post/<int:pk>/update/", PostUpdateView.as_view(), name="post-update"),
+    path("teacher/<int:pk>/update/", TeacherUpdateView.as_view(), name="teacher-update"),
+    path("teacher/<int:pk>/update/",
+         TeacherUpdateView.as_view(), name="teacher-update"),
     path("post/<int:pk>/delete/", PostDeleteView.as_view(), name="post-delete"),
+    path("teacher/<int:pk>/delete/",
+         TeacherDeleteView.as_view(), name="teacher-delete"),
     path("about/", views.about, name="myapp-about"),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -47,6 +47,17 @@ class UserPostListView(ListView):
         return Post.objects.filter(author=user).order_by('-date_posted')
 
 
+class UserTeacherListView(ListView):
+    model = Teacher
+    template_name = 'myapp/teacher_posts.html'    # <app>/<model>_<viewtype>.html
+    context_object_name = 'teachers'
+    paginate_by = 9
+
+    def get_queryset(self):
+        user = get_object_or_404(User, username=self.kwargs.get('username'))
+        return Post.objects.filter(author=user).order_by('-date_posted')
+
+
 class PostDetailView(DetailView):
     model = Post
 
@@ -79,6 +90,21 @@ class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
         return False
 
 
+class TeacherUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Teacher
+    fields = ['teacher_name', 'course_content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        teacher = self.get_object()
+        if self.request.user == teacher.author:
+            return True
+        return False
+
+
 class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Post
     success_url = '/'
@@ -86,6 +112,17 @@ class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     def test_func(self):
         post = self.get_object()
         if self.request.user == post.author:
+            return True
+        return False
+
+
+class TeacherDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Teacher
+    success_url = '/'
+
+    def test_func(self):
+        teacher = self.get_object()
+        if self.request.user == teacher.author:
             return True
         return False
 
